ef532a21fe661ae52068c5f6cb965477a3432ff3
web-lizzard <czarnous.adrian@gmail.com>
2021-02-20 18:50:34 +0100

    Core depedencies
    install core depedencies

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import process from 'process';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
