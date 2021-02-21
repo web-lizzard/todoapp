8d19e9c5738081cd9469f63bab746409c9db6668
web-lizzard <czarnous.adrian@gmail.com>
2021-02-21 11:07:48 +0100

    Start work with task controller
    build task routing and task controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 
+import { taskRouter } from './routes/task.router';
+
 const port = process.env.PORT || 3001;
 
 const app = express();
@@ -11,6 +13,8 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use('/task', taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
